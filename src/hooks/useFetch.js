d609a387a8b0e740d6dce382b7a12420fa4a662d
Lyonict <charles42.h@gmail.com>
2023-08-22 10:14:33 +0200

    Minor improvement to useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,21 +2,20 @@ import { useEffect, useState } from "react";
 
 export default function useFetch(url) {
   const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(null)
 
   useEffect(() => {
-    const fetchData = () => {
-      setIsPending(true)
-      fetch(url)
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => setError(error))
-      setIsPending(false)
-    }
+    return function cleanup() {
+      const fetchData = () => {
+        fetch(url)
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => setError(error))
+      }
 
-    fetchData()
+      fetchData()
+    }
   }, [url]);
 
-  return {data, isPending, error}
+  return {data, error}
 }
